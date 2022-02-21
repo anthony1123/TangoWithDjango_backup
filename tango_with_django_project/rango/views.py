42827207ec3d52c325294abcd450181ac8a264ac
anthony1123 <648512570@qq.com>
2022-02-21 21:35:40 +0000

    Chapter3

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,9 @@
+import rango
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return HttpResponse('Rango says hey there partner!<br><a href=\'/rango/about/\'>About</a>')
+#chapter3
+def about(request):
+    return HttpResponse('Rango says here is the about page.<br><a href=\'/rango/\'>Index</a>')
